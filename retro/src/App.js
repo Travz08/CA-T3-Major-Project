7bb7dbb1716a4f0a3cfc00d69fc63393d12e5c8b
travzk <traviskelekolio@yahoo.com>
2018-01-18 18:16:40 +1100

    add classroom form

--- retro/src/App.js
+++ retro/src/App.js
@@ -2,19 +2,37 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from './actions'
+import * as retroAPI from './api/retrospect'
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header.js'
 import Landing from './components/landing'
 import Dashboard from './components/dashboard.js'
 import Title from './components/log.js'
+import ClassRoomForm from './components/classroom.js'
 import { Row, Col, Card, CardTitle, CardSubtitle, CardText, CardBody, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 
 
 class App extends Component {
   // lifecycle method - as soon as component is rendered on screen go fetch the current user.
   componentDidMount() {
+    retroAPI.all()
+    .then(classrooms => {
+      this.setState({classrooms})
+    })
+
     this.props.fetchUser();
+  }
+
+  handleClassRoomSubmission = (classroom) => {
+    // console.log(title,yearReleased);
+    //give it the previous state and then mutate it
+    this.setState(( {classrooms}) => (
+      { classrooms: [ classroom].concat(classrooms) }
+    ));
+
+
+  retroAPI.save(classroom);
   }
 
   render() {
@@ -29,6 +47,7 @@ class App extends Component {
               </Switch>
             </div>
         </BrowserRouter>
+        <ClassRoomForm onSubmit={this.handleClassRoomSubmission} />
       </div>
     );
   }

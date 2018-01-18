60a1116824e166eb4d7bb6cbc5a137a7fe52ffe8
Syaf Djamil <the.syaf@gmail.com>
2018-01-18 18:15:23 +1100

    refactored API response handling
    Logs are returning, but Posts are not yet.

---
+++ retro/src/components/post.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Row, Col, Card, CardTitle, CardSubtitle, CardText, CardBody, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+
+
+export default function Post({ posts }) {
+    // const { _id, text, classroom_id } = props;
+    // const { title, content, log_id } = posts;
+    console.log('posts: ', posts)
+    return (
+        <div>
+            
+            {
+                !!posts ? (
+                    posts.map(post => (
+                        ('im a post')
+                    ))
+                ) : ('No Posts')
+            }
+        </div>
+        
+        // <Card className="postBox">
+        // <CardBody>
+        //     <CardTitle className="postTitle">Title sdfsdfof the post</CardTitle>
+
+        //     {
+        //     !!posts ? (
+        //         posts.map(post => (
+        //         //   <Post posts={posts} />   
+        //         <div>Yay</div>          
+        //         ))
+        //     ) : ('No posts??')
+        //     }    
+
+        //     <CardSubtitle className="postSubtitle">Challenge</CardSubtitle>
+        //     <CardText className="postContent">The quick brown fox jumped over the lazy dog.</CardText>                                                
+        // </CardBody>
+        // </Card>
+    )
+}

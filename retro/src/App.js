344a52f07c5ab04fabb7399fe47fdd306219e64c
Terence Jeong <terence.jeong@gmail.com>
2018-01-18 18:51:08 +1100

    merged reactlogin into dev

--- retro/src/App.js
+++ retro/src/App.js
@@ -4,22 +4,6 @@ import { connect } from 'react-redux';
 import * as actions from './actions'
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import Header from './components/header.js';
-import Landing from './components/landing';
-import Dashboard from './components/dashboard.js';
-import Classroom from './components/classroom';
-import Title from './components/log.js'
-import { Row, Col, Card, CardTitle, CardSubtitle, CardText, CardBody, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
-
-
-class App extends Component {
-  // lifecycle method - as soon as component is rendered on screen go fetch the current user.
-  componentDidMount() {
-    this.props.fetchUser();
-  }
-
-=======
 import Logs from './components/logs';
 import Post from './components/post';
 
@@ -43,36 +27,21 @@ class App extends Component {
       .then(postData => {
         this.setState({ posts: postData.post })
       })
-    
+
   }
 
 
 
->>>>>>> 3a46d4fc231625c25756049fa13b76c15c1974a0
   render() {
     const { logs, posts } = this.state;
     // console.log(logs, posts)
-    
+
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <BrowserRouter>
-            <div>
-            <Header />
-            <Switch>
-              <Route exact path="/classroom" component={Classroom}></Route>
-              <Route path="/logs" component={Dashboard}></Route>
-              <Route path='/' component={Landing}></Route>
-              </Switch>
-            </div>
-        </BrowserRouter>
-=======
      <div className="App">
         <Logs logs={logs}/>
         <Post posts={posts}/>
-        
 
->>>>>>> 3a46d4fc231625c25756049fa13b76c15c1974a0
+
       </div>
     );
   }

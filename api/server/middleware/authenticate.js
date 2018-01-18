80033f26daae108d08183ec1d6c2ecba37086d3d
Terence Jeong <terence.jeong@gmail.com>
2018-01-18 14:38:42 +1100

    user can now login via google oauth on client side

---
+++ api/server/middleware/authenticate.js
@@ -0,0 +1,19 @@
+// require('../models/user.js');
+//
+// var authenticate = (req, res, next) => {
+//   var token = req.header('cookie');
+//   console.log(token)
+//     if (!token) {
+//         console.log(token)
+//       res.redirect('/class')
+//     }
+//     // req.user = user;
+//     // req.token = token;
+//     next();
+//
+//   // }).catch((e) =>{
+//   //   res.status(401).send()
+//   // })
+// }
+//
+// module.exports = {authenticate}

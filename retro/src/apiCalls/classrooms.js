bbd95745a400b3fad64ee870819d6943a4ec3b65
travzk <traviskelekolio@yahoo.com>
2018-01-19 16:34:57 +1100

    add navbar and links

--- retro/src/apiCalls/classrooms.js
+++ retro/src/apiCalls/classrooms.js
@@ -1,9 +1,9 @@
 
-const retrospectPostsAPI =`https://namenotesapi.herokuapp.com/posts`
-
+const retrospectClassAPI =`https://namenotesapi.herokuapp.com/class`
+const saveClass =`https://namenotesapi.herokuapp.com/class/new`
 
 export function all() {
-    return fetch(retrospectPostsAPI)
+    return fetch(retrospectClassAPI)
         .then(res => res.json())
         // .then((data) => {
         //     console.log(data.logs)
@@ -12,13 +12,13 @@ export function all() {
 
 }
 
-export function store(post) {
-  return fetch('https://namenotesapi.herokuapp.com/post/new', {
+export function save(classroom) {
+  return fetch(saveClass, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(post)
+    body: JSON.stringify(classroom)
   })
   .then(res => res.json())
   .catch(error => {console.log(error)})
